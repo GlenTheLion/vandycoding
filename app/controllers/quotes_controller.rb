459a77da957e5625f4cc2e42a4d960473ef9fef5
Glen Datugan <Glen.datugan@gmail.com>
2019-06-09 21:32:22 +0000

    Added validations for quote input and gives warning sign if the user puts invalid data

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -12,4 +12,25 @@ class QuotesController < ApplicationController
 
   end
 
+  def create 
+
+    @quote = Quote.create(quote_params)
+
+    if @quote.invalid?
+
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+
+    params.require(:quote).permit(:saying, :author)
+
+  end
+
+
 end
