1701c8f19866a3fd733070341b1df7d501ca8427
Glen Datugan <Glen.datugan@gmail.com>
2019-06-09 22:04:10 +0000

    Added about page with photo adjusted top margin of slogan to 20x created paragraphs in the about page of splurty.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   root 'quotes#index'
   resources :quotes
+  get 'about', to: 'quotes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
